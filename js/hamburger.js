ffbd8b21e17a4d78de2b5503d81c78789f61ac75
racingulitka <mex83011@gmail.com>
2024-06-08 13:57:12 +0300

    feat: version 4.0

---
+++ js/hamburger.js
@@ -0,0 +1,32 @@
+﻿window.onload = () => {
+
+	const hamburger = document.querySelector(".header-desktopMenu")
+}
+
+const ulMenu = document.querySelector(".header-desktopMenu")
+const hamburger = document.querySelectorAll('.header-hamburgerIcon-stick')
+const isMobile = getComputedStyle(document.querySelector('.header-logo')).display === 'none'
+
+let isHamburgerOpen = false
+
+const onHamburgerClick = () =>{
+	isHamburgerOpen = !isHamburgerOpen
+	if(isMobile){
+		if(isHamburgerOpen) {
+			ulMenu.style.cssText = 'translate:0% 0%;'
+			for(let i = 0; i < hamburger.length; i++){
+				hamburger[i].style.background = 'var(--mainBackground)'
+			}
+			hamburger[0].style.transformOrigin = 'top left'
+			hamburger[0].style.rotate = '45deg'
+			hamburger[1].style.opacity = 0;
+			hamburger[2].style.transformOrigin = 'bottom left'
+			hamburger[2].style.rotate = '-45deg'
+		} else {
+			ulMenu.style.cssText = 'translate:-100% 0%;'
+			for(let i = 0; i < hamburger.length; i++){
+				hamburger[i].style.cssText = 'background:var(--mainFontColor);'
+			}
+		}
+	}
+}
---
+++ js/main.js
@@ -0,0 +1,51 @@
+﻿const options = {
+	root:null,
+	rootMargin:'-50px',
+	threshold:0.5,
+}
+
+const observerH2 = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting){
+			entry.target.style.opacity = 1;
+			entry.target.style.translate = '0% 0%';
+		}
+	});
+}, options);
+
+const h2 = document.querySelectorAll('h2')
+h2.forEach(item => {
+	observerH2.observe(item)
+})
+
+const observerDoctorBlock = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if(entry.isIntersecting){
+			entry.target.style.scale = 1
+			entry.target.style.opacity = 1
+		}
+	})
+}, options)
+
+const doctorContainerItems = document.querySelectorAll(".doctor-container-item")
+
+
+doctorContainerItems.forEach(item => {
+	observerDoctorBlock.observe(item)
+})
+
+const observerEquipmentBlock = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if(entry.isIntersecting){
+			entry.target.style.scale = 1
+			entry.target.style.opacity = 1
+		}
+	})
+}, options)
+
+const equipmentFlexItems = document.querySelectorAll(".equipment-flex-container")
+
+
+equipmentFlexItems.forEach(item => {
+	observerEquipmentBlock.observe(item)
+})
